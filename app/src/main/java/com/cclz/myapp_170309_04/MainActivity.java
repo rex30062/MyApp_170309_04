a662fca9d1f3ae34822f7861813add551ad46cb1
Cclz <rex620620>
2017-03-09 14:47:13 +0800

    Initial commit

---
+++ app/src/main/java/com/cclz/myapp_170309_04/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cclz.myapp_170309_04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv=(WebView)findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+        wv.getSettings().setJavaScriptEnabled(true);    //  啟用 JacaScript
+        wv.loadUrl("http://www.hinet.net");
+    }
+}
